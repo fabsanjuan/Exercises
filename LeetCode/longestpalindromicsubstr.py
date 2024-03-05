a7450a7912e28bbf8ffd9f4fc3777e259c2a0153
Fabio Sanjuan <fabio.sanjuangomez@gmail.com>
2024-03-05 10:18:54 -0800

    Longest palindromic substring problem

---
+++ LeetCode/longestpalindromicsubstr.py
@@ -0,0 +1,27 @@
+# Problem: Given a string s, return the longest palindromic substring in s.
+# Solution: 
+
+
+
+def longest_palindrome(s):
+    if len(s) == 0:
+        return ""
+    
+    start, end = 0, 0
+    
+    def expand_around_center(left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
+
+    for i in range(len(s)):
+        len1 = expand_around_center(i, i)
+        len2 = expand_around_center(i, i + 1)
+        max_length = max(len1, len2)
+
+        if max_length > end - start:
+            start = i - (max_length - 1) // 2
+            end = i + max_length // 2
+    
+    return s[start:end + 1]
