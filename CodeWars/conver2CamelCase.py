eccfd9d87e73d1c0b3c37d1f94e2a70db3664d2c
Fabio Sanjuan <fabio.sanjuangomez@gmail.com>
2024-03-06 11:01:40 -0800

    Codewars kata: Convert 2 camelCase (6)

---
+++ CodeWars/conver2CamelCase.py
@@ -0,0 +1,21 @@
+# Problem: Convert words in a string delimited by dashes and underscores into camelCase.
+# Solution: Iterate through chars and skip dashes and uncerscores capitalizing the next char. 
+
+def to_camel_case(text):
+    if text == '':
+        return ''
+    
+    camel_case_str = []
+    skip = False
+    for i, char in enumerate(text):
+        if skip:
+            skip = False
+            continue
+        if ((char == '-' or char == '_') and i + 1 < len(text)):
+            char = text[i + 1].upper()
+            camel_case_str.append(char)
+            skip = True
+        else:
+            camel_case_str.append(char)
+
+    return ''.join(camel_case_str)
