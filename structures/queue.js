20f48298c20cd1575d54d3535feb3780ba27f6ac
Fabio Sanjuan <fabio.sanjuangomez@gmail.com>
2024-05-13 20:06:17 -0800

    added data structures

---
+++ structures/queue.js
@@ -0,0 +1,23 @@
+// Queue js implementation.
+
+class Queue {
+    constructor() {
+      this.items = [];
+    }
+  
+    enqueue(item) {
+      this.items.push(item);
+    }
+  
+    dequeue() {
+      return this.items.shift();
+    }
+  
+    peek() {
+      return this.items[0];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  }
