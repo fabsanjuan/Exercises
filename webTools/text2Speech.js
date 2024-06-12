7836b0b76f4e7ca9ed22159bc7dc3f5c02b330a1
Fabio Sanjuan <fabio.sanjuangomez@gmail.com>
2024-06-11 18:58:25 -0700

    added notes and template

--- webTools/text2Speech.js
+++ webTools/text2Speech.js
@@ -3,3 +3,9 @@
 // function formatInput(user_input) {execution block}
 // function parseInput(formatted) {execution block}
 // function audioOutput(parsed) {execution block} 
+
+
+
+
+// Train AIs to summarize content / information of a body of text for perusal of long documents.
+// Go back and summarize academic performance to look for skill specific job opportunities.
